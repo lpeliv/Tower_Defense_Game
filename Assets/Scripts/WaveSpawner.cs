9b52e3ae0871194bcea4869252ed4d7299e54e1e
lpeliv <92396416+lpeliv@users.noreply.github.com>
2024-10-01 23:59:00 +0200

    version 0.0.1

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections;
+using TMPro;
+
+// Spawner needs to be improved, multiple different enemies have to spawn in same wave,
+// add a random spawn location so enemies don't overlap, activate different Wave Spawner
+// according to the wave number
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5.5f;
+    private float countdown = 2.5f;
+    public float timeBetweenEnemies = 0.5f;
+
+    public TextMeshProUGUI waveCountdownText;
+
+    private int waveIndex = 0;
+
+    private void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }    
+
+        countdown -= Time.deltaTime;
+
+        waveCountdownText.text = Mathf.Round(countdown).ToString();
+    }
+
+    // Should be implemented --> array of waves with enemy types and other specifics
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++) 
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(timeBetweenEnemies);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+// This script is for waypoints now, a different pathfinding is going to be used
+
+public class Waypoints : MonoBehaviour
+{
+    public static Transform[] points;
+
+    void Awake()
+    {
+        points = new Transform[transform.childCount];
+        for(int i = 0; i < points.Length; i++)
+        {
+            points[i] = transform.GetChild(i);
+        }
+    }
+}
